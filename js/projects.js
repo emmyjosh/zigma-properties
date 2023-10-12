63e78a4054f06aa298ca7ba81e5f96840fcc8bb7
OGpacatech <emmycloxzy07@gmail.com>
2023-10-12 20:56:39 +0100

    test

--- js/projects.js
+++ js/projects.js
@@ -1,74 +1,58 @@
 /** @format */
 
-let axios = require("axios");
 const fetchDataAndPopulateCarousels = async () => {
   try {
-    const res = await axios.get("http://localhost:5000/api/projects");
+    const response = await fetch("http://localhost:5000/api/projects");
 
-    if (res.status !== 200) {
-      throw new Error(`Failed to fetch data. Status: ${res.status}`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data. Status: ${response.status}`);
     }
 
-    const data = res.data.project; // Check the API response structure
+    const data = await response.json(); // Parse response as JSON
 
-    const displayProjectsCarousel = data.map((project) => {
-      return `
-      
-        <div class="project-item swiper-slide">
-          <img src=${project.imageUrl} alt=${project.category}> <!-- Use imageUrl instead of image -->
-          <div class="overlay"></div>
-          <a href=${project.imageUrl} class="view-icon img-popup" data-gall="myGallery">
-            <i class="fas fa-expand"></i>
-          </a>
-          <div class="projects-content">
-            <a href=${project.imageUrl} class="category">${project.category}</a>
-            <h3><a href=${project.imageUrl} class="title">${project.title}</a></h3> 
-          </div>
-        </div>
-      `;
-    });
+    console.log("HTML has been inserted into projectCarousel.");
 
-    const displayProjects = data.map((project) => {
+    const displayProjects = data.project.map((project) => {
       return `
         <div class="col-lg-4 col-sm-6 padding-15">
-          <div class="project-item">
-            <img src=${project.imageUrl} class="resize-image" alt="projects"> 
+          <div class="project-item swiper-slide">
+            <img src="${project.imageUrl}" alt="${project.title}">
             <div class="overlay"></div>
-            <a href=${project.imageUrl} class="view-icon img-popup"> <i class="fas fa-expand"></i></a>
+            <a href="${project.imageUrl}" class="view-icon img-popup">
+              <i class="fas fa-expand"></i>
+            </a>
             <div class="projects-content">
-              <h2><a href=${project.imageUrl} class="category">${project.category}</a></h2> 
-              <h4><a href=${project.imageUrl} class="title">${project.title}</a></h4>
+              <a href="#" class="category">${project.category}</a>
+              <h3><a href="project-single.html" class="title">${project.title}</a></h3>
             </div>
           </div>
         </div>
       `;
     });
 
-    const projectCarousel = document.getElementById("projectCarousel"); // Check if this element exists in your HTML
-    const project = document.getElementById("project"); // Check if this element exists in your HTML
+    const project = document.getElementById("projectD");
 
-    if (projectCarousel && project) {
-      projectCarousel.innerHTML = displayProjectsCarousel.join("");
-      project.innerHTML = displayProjects.join("");
+    project.innerHTML = displayProjects.join("");
 
-      // Initialize Swiper here after the data is inserted into the carousel container
-      var swiper = new swiper(".mySwiper", {
-        slidesPerView: 1,
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
-    }
+    // Initialize Swiper with pagination
+    var swiper = new Swiper(".swiper-container", {
+      slidesPerView: 1,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
 // Call the fetchDataAndPopulateCarousels function when the DOM is loaded
-fetchDataAndPopulateCarousels();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchDataAndPopulateCarousels();
+});
