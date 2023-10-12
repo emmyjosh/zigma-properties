25181e5dcf01008c60d9377e69ebec240c18a737
OGpacatech <emmycloxzy07@gmail.com>
2023-10-12 22:12:05 +0100

    commit

--- backend/server/controllers/cloudinary.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-const cloudinary = require("cloudinary").v2; // Import the Cloudinary SDK
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
-});
-module.exports = cloudinary;
